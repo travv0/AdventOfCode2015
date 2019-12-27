d7a03b0a1c761052d331ab91c8ee0e6ecdfa3930
Travis <tssund93@gmail.com>
2019-12-27 17:39:15 -0600

    day 3 part 2

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -14,6 +14,20 @@
               [0 0]
               directions))
 
-(defn -main []
+(defn split-directions [directions]
+  (reduce (fn [[santa1 santa2] [dir1 dir2]]
+            [(conj santa1 dir1) (conj santa2 dir2)])
+          [[] []]
+          (partition-all 2 directions)))
+
+(defn -main [part]
   (let [directions (parse-directions (slurp "input.txt"))]
-    (-> directions get-route set count)))
+    (case part
+      1 (-> directions get-route set count)
+      2 (->> directions
+             split-directions
+             (map get-route)
+             (reduce concat)
+             set
+             count)
+      (throw (Exception. "`part` must be 1 or 2")))))
