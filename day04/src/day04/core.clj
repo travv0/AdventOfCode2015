5838dd4f8ca3b1a4b035baf7db5804b5e32ada7d
Travis <tssund93@gmail.com>
2019-12-27 23:34:31 -0600

    day 4

---
+++ day04/src/day04/core.clj
@@ -0,0 +1,24 @@
+(ns day04.core
+  (:import java.security.MessageDigest)
+  (:gen-class))
+
+(defn md5 [^String s]
+  (->> s
+       .getBytes
+       (.digest (MessageDigest/getInstance "MD5"))
+       (BigInteger. 1)
+       (format "%032x")))
+
+(defn lowest-hash-with-leading-zeros [n key]
+  (loop [i 1]
+    (if (every? (partial = \0)
+                (take n (md5 (format "%s%d" key i))))
+      i
+      (recur (inc i)))))
+
+(defn -main [part]
+  (let [key "bgvyzdsv"]
+    (case part
+      1 (lowest-hash-with-leading-zeros 5 key)
+      2 (lowest-hash-with-leading-zeros 6 key)
+      (throw (Exception. "`part` must be 1 or 2")))))
