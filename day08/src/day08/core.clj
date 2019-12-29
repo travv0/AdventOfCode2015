32eb2fb9dcd5f1f60b2da126e079610e8c5d69f6
Travis <tssund93@gmail.com>
2019-12-28 20:03:35 -0600

    day 8 part 2

--- day08/src/day08/core.clj
+++ day08/src/day08/core.clj
@@ -2,18 +2,26 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(defn count-char-difference [s]
-  (reduce #(+ %1 (->> %2
-                      (filter (comp not nil?))
-                      count
-                      dec))
+(defn count-regex-matches [total seq-matches]
+  (+ total (->> seq-matches
+                (filter (comp not nil?))
+                count
+                dec)))
+
+(defn count-decoded-char-difference [s]
+  (reduce count-regex-matches
           0
           (re-seq #"(\"|\\\\|\\\"|(\\x)(..))" s)))
+
+(defn count-encoded-char-difference [s]
+  (reduce count-regex-matches
+          2
+          (re-seq #"(\"|\\|(\\x)(..))" s)))
 
 (defn -main [part]
   (let [input (slurp "input.txt")
         lines (str/split-lines input)]
     (case (str part)
-      "1" (reduce + (map count-char-difference lines))
-      "2" (throw (Exception. "unimplemented"))
+      "1" (reduce + (map count-decoded-char-difference lines))
+      "2" (reduce + (map count-encoded-char-difference lines))
       (throw (Exception. "`part` must be 1 or 2")))))
