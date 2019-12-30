8bfea73f0166241881726f4b5f43ba4cfbb41d3c
Travis <tssund93@gmail.com>
2019-12-29 20:45:55 -0600

    day 12 part 2

--- day12/project.clj
+++ day12/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.json "0.2.7"]
+                 [com.rpl/specter "1.1.3"]]
   :main ^:skip-aot day12.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- day12/src/day12/core.clj
+++ day12/src/day12/core.clj
@@ -1,16 +1,26 @@
 (ns day12.core
+  (:require [clojure.data.json :as json]
+            [com.rpl.specter :as s])
   (:gen-class))
 
 (defn sum-numbers-in-json [json]
   (->> json
-       (re-seq #"-?\d+")
-       (map #(Integer/parseInt %))
+       (s/select (s/walker number?))
        (reduce +)))
 
+(defn contains-red? [map]
+  (when (map? map)
+    (some (comp #(= "red" %) second) map)))
+
+(defn sum-without-reds [json]
+  (->> json
+       (s/transform (s/walker contains-red?) s/NONE)
+       sum-numbers-in-json))
+
 (defn -main [part]
-  (let [input (slurp "input.txt")]
+  (let [json (json/read-str (slurp "input.txt"))]
     (println
      (case (str part)
-       "1" (sum-numbers-in-json input)
-       "2" (throw (Exception. "unimplemented"))
+       "1" (sum-numbers-in-json json)
+       "2" (sum-without-reds json)
        (throw (Exception. "`part` must be 1 or 2"))))))
