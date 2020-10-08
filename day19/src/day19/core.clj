d22c2d1299d971c193e4fd6021762bb10108c5e9
Travis <tssund93@gmail.com>
2020-10-08 12:35:55 -0500

    day 19 part 2

--- day19/src/day19/core.clj
+++ day19/src/day19/core.clj
@@ -22,11 +22,20 @@
 (defn parse-molecule [input]
   (last (str/split-lines input)))
 
+(defn find-fewest-steps-from-e [molecule replacements]
+  (letfn [(f [molecule replacements step]
+            (if (= molecule "e")
+              step
+              (->> (find-replacements molecule replacements)
+                   (map #(f % replacements (inc step)))
+                   (some identity))))]
+    (f molecule (map reverse replacements) 0)))
+
 (defn -main [part]
   (let [input (slurp "input.txt")
         replacements (parse-replacements input)
         molecule (parse-molecule input)]
     (case part
       1 (count (find-replacements molecule replacements))
-      2 (throw (Exception. "part 2 not implemented"))
+      2 (find-fewest-steps-from-e molecule replacements)
       (throw (Exception. "`part` must be 1 or 2")))))
